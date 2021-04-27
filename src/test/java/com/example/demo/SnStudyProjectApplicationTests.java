15bbe97bf5a15170d431d74bcfaf31c0511856b8
amjay <amjay.31@gmail.com>
2021-04-27 19:10:26 +0900

    test 코드 작성

--- src/test/java/com/example/demo/SnStudyProjectApplicationTests.java
+++ src/test/java/com/example/demo/SnStudyProjectApplicationTests.java
@@ -2,32 +2,61 @@ package com.example.demo;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import sn.example.demo.SnStudyProjectApplication;
+import sn.example.demo.controller.PpurigiController;
 
-@SpringBootTest
-class SnStudyProjectApplicationTests {
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest(classes = SnStudyProjectApplication.class)
+@AutoConfigureMockMvc
+public class SnStudyProjectApplicationTests {
 
 	@Autowired
-	MockMvc mockMvc;
+	private MockMvc mockMvc;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Test
+	void test() throws Exception{
+		mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+				.andExpect(status().isOk());
+
+	}
 	
 	@Test
 	void send() throws Exception {
 		//given
-		MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-		params.add("sendUserId", "hannah");
-		params.add("amount", "1000");
-		params.add("reqCnt", "2");
-		
+		String url = "/ppurigi";
+		Map<String, Object> params = new HashMap<>();
+		params.put("sendUserId", "hannah");
+		params.put("amount", 1000);
+		params.put("reqCnt", 2);
+		String content = objectMapper.writeValueAsString(params);
+
 		//when
-		mockMvc.perform(post("/send").params(params))
-				.andDo(print());
-		
+		mockMvc.perform(MockMvcRequestBuilders.post(url)
+				.content(content)
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
 		//then
 	}
 
